8c9fa42a2d5c9eae6e6555810bec81d477a91274
Sanwor Rajbhandari <39193994+Sanwor@users.noreply.github.com>
2026-02-02 13:09:30 +0545

    Update custom_textformfield.dart

--- custom_textformfield.dart
+++ custom_textformfield.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:get/get.dart';
-import 'package:syuseiclub/src/app_config/app_styles.dart';
 
 class CustomTextFormField extends StatelessWidget {
   final String? headingText;
@@ -257,4 +256,5 @@ class CustomTextFormField extends StatelessWidget {
       ],
     );
   }
+
 }
