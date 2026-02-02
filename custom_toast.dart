63bf3836c9c4f71264099c8c2d39c039c4afdc0a
Sanwor Rajbhandari <39193994+Sanwor@users.noreply.github.com>
2026-02-02 14:14:35 +0545

    snackbar

---
+++ custom_toast.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+showToast(message) {
+  return Get.snackbar(
+    "Success",
+    message,
+    backgroundColor: Colors.green.shade700,
+    colorText: Colors.white,
+    icon: Icon(Icons.check_circle, color: Colors.white),
+    snackPosition: SnackPosition.TOP,
+    duration: Duration(seconds: 3),
+    margin: EdgeInsets.all(10),
+    borderRadius: 8,
+  );
+}
+
+showErrorToast(String message) {
+  return Get.snackbar(
+    "Error",
+    message,
+    backgroundColor: Colors.red.shade700,
+    colorText: Colors.white,
+    icon: Icon(Icons.error_outline, color: Colors.white),
+    shouldIconPulse: true,
+    duration: Duration(seconds: 3),
+    snackPosition: SnackPosition.TOP,
+    margin: EdgeInsets.all(10),
+    borderRadius: 8,
+  );
+}
